a56f177c886addf528a9115e85c2df4ce4947bf5
redcomet88 <626206333@qq.com>
2019-02-17 13:49:25 +0800

    路由

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -16,6 +16,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
