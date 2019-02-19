38819a2097b91d8729c6ef73aff6216804892643
redcomet88 <626206333@qq.com>
2019-02-19 10:58:36 +0800

    HTTP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+// Routes tell the router which view to display
+// when a user clicks a link or pastes a URL into the browser address bar.
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
@@ -12,6 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
+  // The method is called forRoot() because you configure the router at the application's root level
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ]
 })
